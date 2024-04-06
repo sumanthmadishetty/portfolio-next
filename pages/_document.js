223e2c41ae4702e05443b1c9d09340835770815e
Sumanth Madishetty <mailme@sumanth.tech>
2024-04-06 21:02:43 +0530

    fix: Responsive Issues in mobile

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
             href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
             rel="stylesheet"
           />
-          <title>Sumanth Madishetty</title>
+
           <meta name="description" content="Sumanth Madishetty" />
           {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script> */}
