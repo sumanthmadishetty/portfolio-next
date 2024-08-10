989606ef65ee6a9d30a6ff3d75f96ebd20e06d5f
Sumanth Madishetty <mailme@sumanth.tech>
2024-08-10 20:42:40 +0530

    feat: added gtm

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,32 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WLTFBWFD');
+          `,
+            }}
+          ></script>
+
           <meta name="description" content="Sumanth Madishetty" />
           {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script> */}
         </Head>
         <body style={{ maxWidth: "1500px", margin: "auto" }}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WLTFBWFD"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
