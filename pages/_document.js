d99b8a744c975ac6181a7f36524888b515746bd6
Sumanth Madishetty <mailme@sumanth.tech>
2025-02-02 20:26:36 +0530

    feat: Google Analytics integration

--- pages/_document.js
+++ pages/_document.js
@@ -16,21 +16,20 @@ class MyDocument extends Document {
           />
 
           <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-E3JVDXBKRB"
-          ></script>
-
-          <script
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-E3JVDXBKRB');`,
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WLTFBWFD');
+          `,
             }}
           ></script>
 
           <meta name="description" content="Sumanth Madishetty" />
+          {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script> */}
         </Head>
         <body style={{ maxWidth: "1500px", margin: "auto" }}>
           <noscript>
