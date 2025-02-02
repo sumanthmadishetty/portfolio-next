dbeab152a2af0dd5ae3089ddef343b37552e3537
Sumanth Madishetty <mailme@sumanth.tech>
2025-02-02 20:22:43 +0530

    feat: Google Analytics integration

--- pages/_document.js
+++ pages/_document.js
@@ -21,11 +21,13 @@ class MyDocument extends Document {
           ></script>
 
           <script
-            dangerouslySetInnerHTML={`window.dataLayer = window.dataLayer || [];
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-E3JVDXBKRB');`}
+  gtag('config', 'G-E3JVDXBKRB');`,
+            }}
           ></script>
 
           <meta name="description" content="Sumanth Madishetty" />
